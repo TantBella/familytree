972a4dd8dca5c13413b2e4573b94379e792b1cb7
Bella Solo <bella.solomon-falkfrii@outlook.com>
2023-11-04 14:26:15 +0100

    Add second version

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,27 @@ const express = require("express"),
 
 const app = express();
 
-app.get("/api", (_request, response) => {
-  response.send({ hello: "World" });
+const dotenv = require("dotenv"),
+  { Client } = require("pg");
+
+dotenv.config();
+
+const client = new Client({
+  connectionString: process.env.PGURI,
+});
+
+client.connect();
+
+// app.get("/api", (_request, response) => {
+//   response.send({ hello: "World" });
+// });
+
+app.get("/api", async (_request, response) => {
+  const { rows } = await client.query("SELECT * FROM cities WHERE name = $1", [
+    "Stockholm",
+  ]);
+
+  response.send(rows);
 });
 
 app.use(express.static(path.join(path.resolve(), "dist")));
